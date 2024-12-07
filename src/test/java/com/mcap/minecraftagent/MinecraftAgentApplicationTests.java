a904384824ec19a5d3eba322aab667664a571e77
youssif-k97 <youssif.kmayra97@gmail.com>
2024-12-07 23:17:54 +0100

    added dockerfile and jenkinsfile

---
+++ src/test/java/com/mcap/minecraftagent/MinecraftAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcap.minecraftagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
